03f4bf912b9b506b7033e276328182dad9530bf9
sscodes <sanketcrj@gmail.com>
2022-06-15 20:28:53 +0530

    project 3 changes

--- src/subcomponent/GradientButton.jsx
+++ src/subcomponent/GradientButton.jsx
@@ -3,7 +3,7 @@ import '../subcomponent_styles/GradientButton.scss';
 const GradientButton = () => {
   return (
     <>
-      <button className='button-style px-10 py-4 rounded-full shadow-lg shadow-pink-200/50'>
+      <button className='button-style px-10 py-4 btn-shadow'>
         <h1 className='text-black text-xl'>View Submission</h1>
       </button>
     </>
