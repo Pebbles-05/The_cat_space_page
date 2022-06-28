75629567d8dbfdedae7186b8350f36c40b2dc689
pebbles <you@example.com>
2022-06-27 23:06:13 -0800

    updates on blob

--- src/components/blob.jsx
+++ src/components/blob.jsx
@@ -1,7 +1,10 @@
 import {useState,useEffect,useRef} from 'react';
 import useMousePosition from '../hooks/useMousePosition';
+import useWindowSize from "../hooks/useWindowSize";
 
-export default function Blob({isbig}) {
+export default function Blob() {
+
+  const windowsize = useWindowSize();
 
 // using state for controling imgdata
 const[dimensions,setdimensions]=useState({height:0,width:0});
@@ -29,9 +32,16 @@ useEffect(() => {
 
     window.addEventListener("resize",checkdimensions);
 }, [])
-    const {x,y}=useMousePosition();
+  
+
+const {x,y}=useMousePosition();
+
+
+
+
+
   return (
-    <div ref={dimRef} className='cursor__blob' style={{left:`${x-dimensions.width/2}px`,top:`${y-dimensions.height/2}px`,transform:`scale(${isbig?2:1})`}}>
+    <div ref={dimRef} className='cursor__blob' style={windowsize.width>=1000 ? {left:`${ x - dimensions.width /2}px`,top:`${y-dimensions.height/2}px`,transform:'scale(1)'} : {left:"0",top:"0",transform:'scale(0)'}}>
 
 
     </div>
