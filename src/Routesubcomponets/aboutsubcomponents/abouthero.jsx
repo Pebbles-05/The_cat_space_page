6a667b2c4df7810254de3155a04b6acd6e3887a4
pebbles <you@example.com>
2022-07-01 17:43:36 -0800

    u

--- src/Routesubcomponets/aboutsubcomponents/abouthero.jsx
+++ src/Routesubcomponets/aboutsubcomponents/abouthero.jsx
@@ -30,12 +30,12 @@ export default function Abouthero() {
   return (
     <div className='abouthero'>
         <div className="abouthero__profile">
-          <div className="bg" data-aos="fade-up" data-aos-duration="500" data-aos-once="true"> <img loading='eager' style={{transform:`translateY(${scroll * -150}%)`}} src="https://ik.imagekit.io/thecatspace/catspace_media/the_picturesfinal.png" alt="profilebg" /></div>
+          <div className="bg" data-aos="fade-up" data-aos-duration="500" data-aos-once="true"> <img loading='eager' style={{transform:`translateY(${scroll * -150}%)`}} src="https://ik.imagekit.io/thecatspace/catspace_media/testing/abtest.png" alt="profilebg" /></div>
             
             <div className='pp'  > <img data-aos="fade" data-aos-duration="1000" data-aos-once="true" src="https://ik.imagekit.io/thecatspace/catspace_media/hoverimg1.png" alt="profilepic" /></div>
         </div>
 
-      <div className="abouthero__bio"><p data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Hi I am Ishika a UX and Visual designer based in India. I started learning UX a year back from a course in Internshala and I instantly fell in love with it.</p></div>
+      <div className="abouthero__bio"><p data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Hi I am Ishika a UX and Visual designer based in India. I started learning UX almost a year back from a course in Internshala and I instantly fell in love with it.</p></div>
 
     </div>
   )
